7a3fc1bf2e7f0603e933aef01bafb4933bfe4d7a
James Gowans <jgowans@amazon.com>
2020-08-27 18:39:34 +0200

    c++ singleton example showing constructor invoke

---
+++ cpp-singleton/main.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+
+class S
+{
+    public:
+        static S& getInstance()
+        {
+		std::cout << "top of getInstance()\n";
+            static S    instance; // Guaranteed to be destroyed.
+                                  // Instantiated on first use.
+	std::cout << "About to return\n";
+            return instance;
+        }
+    private:
+        S() {
+		std::cout << "hello from constructor\n";
+	
+	}                    // Constructor? (the {} brackets) are needed here.
+
+        // C++ 03
+        // ========
+        // Don't forget to declare these two. You want to make sure they
+        // are inaccessible(especially from outside), otherwise, you may accidentally get copies of
+        // your singleton appearing.
+        //S(S const&);              // Don't Implement
+        //void operator=(S const&); // Don't implement
+
+        // C++ 11
+        // =======
+        // We can use the better technique of deleting the methods
+        // we don't want.
+    public:
+        S(S const&)               = delete;
+        void operator=(S const&)  = delete;
+
+        // Note: Scott Meyers mentions in his Effective Modern
+        //       C++ book, that deleted functions should generally
+        //       be public as it results in better error messages
+        //       due to the compilers behavior to check accessibility
+        //       before deleted status
+};
+
+
+int main(int argc, char **argv) {
+	std::cout << "hello world\n";
+	S &s = S::getInstance();
+	return 0;
+}
