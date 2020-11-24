d5c6e97893a76b2a8fb4b8609adac3cae554a5a4
James Gowans <jgowans@amazon.com>
2020-11-24 22:31:37 +0200

    Memory bandwidth measure is plottable

--- memory-bandwidth/memory-access-plotter.py
+++ memory-bandwidth/memory-access-plotter.py
@@ -5,18 +5,36 @@ import matplotlib.pyplot as plt
 import struct
 import sys
 
-NS_TO_SEC = 1000 * 1000 * 1000
+NS_TO_SEC = 1000.0 * 1000 * 1000
+NS_TO_MS = 1000 * 1000
 
-with open(sys.argv[1], mode='rb') as f:
-    x = []
-    y = []
-    wall_start = 0;
-    last_user = 0;
-    for wc_s, wc_ns, user in struct.iter_unpack("LLL", f.read()):
-        if (wall_start == 0):
-            wall_start = (wc_s * NS_TO_SEC) + wc_ns
-        x.append((wc_s * NS_TO_SEC) + wc_ns - wall_start)
-        y.append(user - last_user)
-        last_user = user
-    plt.plot(x, y)
+f, axes = plt.subplots(2, 1, sharey=True, sharex=True)
+
+start_ts = sorted([int(s.split("/")[-1].split("-")[0]) for s in sys.argv[1:]])[0]
+print("Start ts: {}".format(start_ts))
+wall_start = start_ts * 1000
+
+for plot_idx, filename in enumerate(sys.argv[1:]):
+    with open(filename, mode='rb') as f:
+        plot_idx = 0
+        x = []
+        y = []
+        wall_start = 0;
+        last_user = 0;
+        idx = 0;
+        for wc_s, wc_ns, user in struct.iter_unpack("LLL", f.read()):
+            x.append((((wc_s * NS_TO_SEC) + wc_ns) / NS_TO_MS) - wall_start)
+            if last_user == 0:
+                y.append(0)
+            else:
+                y.append(float(user - last_user) / 1000)
+            last_user = user
+            idx += 1;
+            if idx == 100:
+                #axes[plot_idx].axvline((((wc_s * NS_TO_SEC) + wc_ns) / NS_TO_MS) - wall_start, color = 'r')
+                idx = 0
+        axes[plot_idx].plot(x, y)
+        axes[plot_idx].set_ylabel("milliseconds")
+        axes[plot_idx].set_xlabel("milliseconds")
+        axes[plot_idx].set_title(filename.split("/")[-1])
 plt.show()
--- memory-bandwidth/simple-memory.c
+++ memory-bandwidth/simple-memory.c
@@ -9,21 +9,42 @@
 
 // The smaller ones need to divide into the bigger ones.
 #define BUF_SIZE (1000UL * 1024 * 1024)
-#define MEASURE_CHUNK_SIZE (10UL * 1024 * 1024)
 #define CACHE_LINE 64
+// Chunk this many accesses into a measurement.
+// (MEASUREMENT_ACCESSES * CACHE_LINE) should divide into BUF_SIZE to keep our
+// loops easy below.
+#define MEASUREMENT_ACCESSES 163840
+// Just so happens to come out as 100.... :-)
+#define MEASUREMENTS_PER_ITER (BUF_SIZE / (MEASUREMENT_ACCESSES * CACHE_LINE))
 
+#define INDEXES (MEASUREMENTS_PER_ITER * MEASUREMENT_ACCESSES)
+#define PAGE_SIZE 4096
 #define ITERATIONS 100
+
+#define RANDOM_ACCESS 0
 
 int main () {
 	clock_t userspace;
 	struct timespec wall;
 	// [wall_sec, wall_ns, userspace_someting]
-	unsigned long measurements[3 * (BUF_SIZE / MEASURE_CHUNK_SIZE)];
+	unsigned long measurements[3 * MEASUREMENTS_PER_ITER];
 	int measurement_idx = 0;
 
 	char *buf = malloc(BUF_SIZE);
-	if (!buf)
+	unsigned int *idx_arr = malloc(INDEXES * sizeof(*idx_arr));
+	if (!buf || !idx_arr)
 		return -ENOMEM;
+
+	for(int i = 0; i < INDEXES; ++i)
+#if RANDOM_ACCESS
+		idx_arr[i] = rand() % BUF_SIZE;
+#else
+		idx_arr[i] = i * CACHE_LINE; // technically should mod BUF_SIZE....
+#endif
+
+	// pre-fault
+	for(int i = 0; i < BUF_SIZE; i += PAGE_SIZE)
+		buf[i] = 0;
 
 	clock_gettime(CLOCK_REALTIME, &wall);
 	char file_name[64];
@@ -35,13 +56,13 @@ int main () {
 	}
 
 	for(int iteration = 0; iteration < ITERATIONS; ++iteration) {
+		clock_t iteration_userspace = clock();
 		int i = 0;
-		while(i < BUF_SIZE) {
-
-			for(int j = i; j < i + MEASURE_CHUNK_SIZE; j += CACHE_LINE) {
-				buf[j] += 1;
+		while(i < (MEASUREMENTS_PER_ITER * MEASUREMENT_ACCESSES)) {
+			for(int j = i; j < i + MEASUREMENT_ACCESSES; ++j) {
+				buf[idx_arr[j]] += 1;
 			}
-			i += MEASURE_CHUNK_SIZE;
+			i += MEASUREMENT_ACCESSES;
 
 			userspace = clock();
 			clock_gettime(CLOCK_REALTIME, &wall);
@@ -50,17 +71,19 @@ int main () {
 			measurements[measurement_idx++] = wall.tv_nsec;
 			measurements[measurement_idx++] = userspace;
 #if 0
-			printf("wall time %lu s\n", measurements[measurement_idx - 2]);
+			printf("wall time %lu s\n", measurements[measurement_idx - 3]);
 			printf("clock() time: %lu s\n", measurements[measurement_idx - 1]);
 #endif
 
 		}
 		measurement_idx = 0;
+		printf("iter clock() time: %lu s\n", clock() - iteration_userspace);
 		int rc = write(measure_log_fd, measurements, sizeof(measurements));
 		if (rc == -1) {
 			printf("write failed with %i\n", errno);
 			return -errno;
 		}
+		printf("iter: %i\n", iteration);
 	}
    	return 0;
 }
